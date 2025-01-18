9d6aece9a1971ae575811d83036f95a18581be98
bruno.duarte <brunomacielduarte32@gmail.com>
2025-01-18 11:16:38 -0300

    add coverage on git ignore

--- tests/pypixcode/builders/test_pix.py
+++ tests/pypixcode/builders/test_pix.py
@@ -7,12 +7,12 @@ class TestPixBuilder(unittest.TestCase):
 
     def test_success_get_code(self):
         # Arrange
-        name = "Bruno Maciel Duarte"
+        name = "Fulano De Tau"
         pix_key = "42fbd387-e918-48c7-abd9-13958bea32ce"
         value = "1.00"
         city = "Para De Minas"
         tx_id = "***"
-        expected_crc = "76D3"  # CRC esperado para o payload fornecido
+        expected_crc = "5039"  # CRC esperado para o payload fornecido
 
         # Cria uma instância do PixBuilder
         pix_builder = PixBuilder(name, pix_key, value, city, tx_id)
